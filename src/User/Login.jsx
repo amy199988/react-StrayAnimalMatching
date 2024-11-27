d83710fa3a88a71b6206ff1df7fdba6c3e7ce6b8
Vivy <amy199988@gmail.com>
2024-11-27 12:10:18 +0800

    20241127

--- src/User/Login.jsx
+++ src/User/Login.jsx
@@ -1,153 +1,77 @@
-import React from 'react'
-import {
-  AlipayCircleOutlined,
-  LockOutlined,
-  MobileOutlined,
-  TaobaoCircleOutlined,
-  UserOutlined,
-  WeiboCircleOutlined,
-} from '@ant-design/icons';
+import React from "react";
+import { LockOutlined, UserOutlined, UserAddOutlined } from "@ant-design/icons";
 import {
   LoginForm,
   ProConfigProvider,
-  ProFormCaptcha,
   ProFormCheckbox,
   ProFormText,
-  setAlpha,
-} from '@ant-design/pro-components';
-import { Space, Tabs, message, theme } from 'antd';
-import { useState } from 'react';
+} from "@ant-design/pro-components";
+import { theme, Button, Space } from "antd";
+import Cat from "../components/icons/cat-space.png";
 
 const Login = () => {
   const { token } = theme.useToken();
-  const [loginType, setLoginType] = useState('phone');
-
-  const iconStyles = {
-    marginInlineStart: '16px',
-    color: setAlpha(token.colorTextBase, 0.2),
-    fontSize: '24px',
-    verticalAlign: 'middle',
-    cursor: 'pointer',
-  };
 
   return (
     <ProConfigProvider hashed={false}>
       <div style={{ backgroundColor: token.colorBgContainer }}>
         <LoginForm
-          logo="https://github.githubassets.com/favicons/favicon.png"
-          title="Github"
-          subTitle="全球最大的代码托管平台"
+          logo={Cat}
+          title="浪浪相親網站"
+          subTitle="Stray Animal Matching"
+          submitter={{
+            searchConfig: {
+              submitText: "登入", // 修改按鈕文字
+            },
+          }}
           actions={
             <Space>
-              其他登录方式
-              <AlipayCircleOutlined style={iconStyles} />
-              <TaobaoCircleOutlined style={iconStyles} />
-              <WeiboCircleOutlined style={iconStyles} />
+              <Button icon={<UserAddOutlined />}>註冊會員</Button>
             </Space>
           }
         >
-          <Tabs
-            centered
-            activeKey={loginType}
-            onChange={(activeKey) => setLoginType(activeKey)}
-          >
-            <Tabs.TabPane key="account" tab="账号密码登录" />
-            <Tabs.TabPane key="phone" tab="手机号登录" />
-          </Tabs>
-          {loginType === 'account' && (
-            <>
-              <ProFormText
-                name="username"
-                fieldProps={{
-                  size: 'large',
-                  prefix: <UserOutlined className={'prefixIcon'} />,
-                }}
-                placeholder={'用户名: admin or user'}
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入用户名!',
-                  },
-                ]}
-              />
-              <ProFormText.Password
-                name="password"
-                fieldProps={{
-                  size: 'large',
-                  prefix: <LockOutlined className={'prefixIcon'} />,
-                }}
-                placeholder={'密码: ant.design'}
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入密码！',
-                  },
-                ]}
-              />
-            </>
-          )}
-          {loginType === 'phone' && (
-            <>
-              <ProFormText
-                fieldProps={{
-                  size: 'large',
-                  prefix: <MobileOutlined className={'prefixIcon'} />,
-                }}
-                name="mobile"
-                placeholder={'手机号'}
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入手机号！',
-                  },
-                  {
-                    pattern: /^1\d{10}$/,
-                    message: '手机号格式错误！',
-                  },
-                ]}
-              />
-              <ProFormCaptcha
-                fieldProps={{
-                  size: 'large',
-                  prefix: <LockOutlined className={'prefixIcon'} />,
-                }}
-                captchaProps={{
-                  size: 'large',
-                }}
-                placeholder={'请输入验证码'}
-                captchaTextRender={(timing, count) => {
-                  if (timing) {
-                    return `${count} ${'获取验证码'}`;
-                  }
-                  return '获取验证码';
-                }}
-                name="captcha"
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入验证码！',
-                  },
-                ]}
-                onGetCaptcha={async () => {
-                  message.success('获取验证码成功！验证码为：1234');
-                }}
-              />
-            </>
-          )}
+          <ProFormText
+            name="username"
+            fieldProps={{
+              size: "large",
+              prefix: <UserOutlined className={"prefixIcon"} />,
+            }}
+            placeholder={"帳號：account"}
+            rules={[
+              {
+                required: true,
+                message: "請輸入帳號！",
+              },
+            ]}
+          />
+          <ProFormText.Password
+            name="password"
+            fieldProps={{
+              size: "large",
+              prefix: <LockOutlined className={"prefixIcon"} />,
+            }}
+            placeholder={"密碼：password"}
+            rules={[
+              {
+                required: true,
+                message: "請輸入密碼！",
+              },
+            ]}
+          />
           <div
             style={{
               marginBlockEnd: 24,
             }}
           >
             <ProFormCheckbox noStyle name="autoLogin">
-              自动登录
+              自動登入
             </ProFormCheckbox>
             <a
               style={{
-                float: 'right',
+                float: "right",
               }}
             >
-              忘记密码
+              忘記密碼
             </a>
           </div>
         </LoginForm>
